05293c0b85824bebd999ac960044a33b8b9a7f4d
Utibe Ntuen <chichipepperz@gmail.com>
2026-04-19 12:31:36 +0100

    initial commit

--- Q5.js
+++ Q5.js
@@ -1,5 +1,5 @@
 //Use the map() method on the numbers array below to create a NEW array where every number is doubled. Log the new array. Do NOT change the original.
 
- const numbers = [3, 6, 9, 12, 15];
+const numbers = [3, 6, 9, 12, 15];
 let doubleNumbers = numbers.map(numbers => numbers * 2) 
 console.log(doubleNumbers)
